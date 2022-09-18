0c0fbee9589a910822ce6afbe1f3be3e30843d7c
Aitolkun <totita@list.ru>
2022-09-19 00:24:45 +0600

    ovalView

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = CGFloat(redView.frame.height) / 2.0
+        yellowView.layer.cornerRadius = CGFloat(yellowView.frame.height) / 2.0
+        greenView.layer.cornerRadius = CGFloat(greenView.frame.height) / 2.0
+        startButton.layer.cornerRadius = 12
     }
-
-
+    
+    @IBAction func startButtonDidNext() {
+    }
+    
 }
 
